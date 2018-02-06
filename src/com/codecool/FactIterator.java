2b7b5d0e63eaee674e9b505d47ffe6740ac5af53
S. Dominik <coderwilk@gmail.com>
2018-02-06 14:39:44 +0100

    Implemented functions

--- src/com/codecool/FactIterator.java
+++ src/com/codecool/FactIterator.java
@@ -1,13 +1,26 @@
 package codecool;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class FactIterator implements Iterator<Fact> {
+
+    private LinkedList<Fact> facts;
+    private int index = 0;
+
+    public FactIterator(LinkedList<Fact> facts) {
+        this.facts = facts;
+    }
+
     public boolean hasNext() {
-        return false;
+        return index < facts.size();
     }
 
     public Fact next() {
-        return null;
+        return facts.get(index++);
+    }
+
+    public void remove() {
+        // Just here... Don't mind me...
     }
 }

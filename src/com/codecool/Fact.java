03c602f7a64c0c6f50c6ab9af2664a6cdc2b6612
S. Dominik <coderwilk@gmail.com>
2018-02-06 12:06:19 +0100

    Added prints and getteres

--- src/com/codecool/Fact.java
+++ src/com/codecool/Fact.java
@@ -1,15 +1,19 @@
 package codecool;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class Fact {
     String id;
     String description;
 
-    public Fact(String id, String description, ) {
+    Map<String, Boolean> genres;
+
+    public Fact(String id, String description, HashMap<String, Boolean> genres) {
         this.id = id;
         this.description = description;
-
+        this.genres = genres;
     }
 
     public Set<String> getIdSet(){
@@ -22,7 +26,16 @@ public class Fact {
     public boolean getValueById(String id){
         return true;
     }
+    public String getId () {
+        return id;
+    }
     public String getDescription(){
-        return "";
+        return description;
+    }
+
+    public void printGenres () {
+        for (Map.Entry<String, Boolean> genre : genres.entrySet()) {
+            System.out.println(genre.getKey() + (genre.getValue() ? "true" : "false"));
+        }
     }
 }

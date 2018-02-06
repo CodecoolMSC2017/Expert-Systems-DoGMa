8adf83f7e824a348a0644ac96c192219d6a9a075
S. Dominik <coderwilk@gmail.com>
2018-02-06 21:13:08 +0100

    Reworked it

--- src/com/codecool/XMLParser.java
+++ src/com/codecool/XMLParser.java
@@ -1,8 +1,23 @@
 package codecool;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 import java.util.LinkedList;
 
 public abstract class XMLParser {
 
-    public abstract LinkedList<Fact> loadXmlDocument(String xmlPath);
+    protected Document document;
+
+    protected void loadXmlDocument(String xmlPath) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            document = dBuilder.parse(new File(xmlPath));
+        } catch (Exception e) {}
+    }
+
+    protected void readElementsFromXml (String xmlPath) {}
 }

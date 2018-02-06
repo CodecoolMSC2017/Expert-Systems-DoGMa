d6d14c09469d9fba944544d2b51041ec0c0a51df
S. Dominik <coderwilk@gmail.com>
2018-02-06 12:05:56 +0100

    Added testing parts

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -3,5 +3,8 @@ package codecool;
 public class Main {
     public static void main(String[] args) {
         System.out.println();
+
+        FactParser fp = new FactParser();
+        fp.printFacts(fp.loadXmlDocument("src/com/codecool/data/series.xml"));
     }
 }

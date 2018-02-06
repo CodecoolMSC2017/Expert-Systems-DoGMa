fad8b215582ddfc719b197ce99a37f431fe3533e
S. Dominik <coderwilk@gmail.com>
2018-02-06 14:40:57 +0100

    Implemented functions

--- src/com/codecool/QuestionIterator.java
+++ src/com/codecool/QuestionIterator.java
@@ -1,13 +1,25 @@
 package codecool;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 
 public class QuestionIterator implements Iterator<Question> {
+    private LinkedList<Question> questions;
+    private int index = 0;
+
+    public QuestionIterator(LinkedList<Question> questions) {
+        this.questions = questions;
+    }
+
     public boolean hasNext() {
-        return false;
+        return index < questions.size();
     }
 
     public Question next() {
-        return null;
+        return questions.get(index++);
+    }
+
+    public void remove() {
+        // Just here... Don't mind me...
     }
 }

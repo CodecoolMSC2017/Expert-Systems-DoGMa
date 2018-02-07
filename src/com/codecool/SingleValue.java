0c65ea19b661d667c8efc07bb6d8dfab37145d3a
S. Dominik <coderwilk@gmail.com>
2018-02-07 11:56:20 +0100

    Removed addInputPattern and param variable

--- src/com/codecool/SingleValue.java
+++ src/com/codecool/SingleValue.java
@@ -1,15 +1,16 @@
 package codecool;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class SingleValue extends Value {
 
-    private String param;
     private boolean selectionType;
 
     public SingleValue(String param, boolean selectionType) {
-        this.param = param;
+        inputPattern.add(param);
         this.selectionType = selectionType;
+        inputPattern = new LinkedList<String>();
     }
 
     public List<String> getInputPattern() {

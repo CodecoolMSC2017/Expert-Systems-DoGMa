be83bfc016ab6d58f06aae188ac64e61061a4520
S. Dominik <coderwilk@gmail.com>
2018-02-07 11:56:40 +0100

    Added constructor

--- src/com/codecool/Question.java
+++ src/com/codecool/Question.java
@@ -7,6 +7,11 @@ public class Question {
     private String question;
     private Answer answer;
 
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
 
 
     public String getId() {

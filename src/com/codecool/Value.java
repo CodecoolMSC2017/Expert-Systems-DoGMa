1510ebe368af2cb1f432403c3bc13713a7b72765
S. Dominik <coderwilk@gmail.com>
2018-02-07 11:56:02 +0100

    Removed addInputPattern and param variable

--- src/com/codecool/Value.java
+++ src/com/codecool/Value.java
@@ -1,8 +1,15 @@
 package codecool;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class Value {
+
+    protected List<String> inputPattern;
+
+    public Value() {
+        inputPattern = new LinkedList<String>();
+    }
 
     public abstract List<String> getInputPattern();
 

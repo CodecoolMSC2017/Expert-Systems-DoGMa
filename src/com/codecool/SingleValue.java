6b51310597ad4ec7d27b333aae7feec14af6ce6c
S. Dominik <coderwilk@gmail.com>
2018-02-07 09:03:04 +0100

    Made simple structure

--- src/com/codecool/SingleValue.java
+++ src/com/codecool/SingleValue.java
@@ -1,6 +1,22 @@
 package codecool;
 
+import java.util.List;
+
 public class SingleValue extends Value {
-    public SingleValue(String param) {
+
+    private String param;
+    private boolean selectionType;
+
+    public SingleValue(String param, boolean selectionType) {
+        this.param = param;
+        this.selectionType = selectionType;
+    }
+
+    public List<String> getInputPattern() {
+        return null;
+    }
+
+    public boolean getSelectionType() {
+        return selectionType;
     }
 }

1f6ac57cdecb06a0341e020319eb0c02ac0c0fb7
S. Dominik <coderwilk@gmail.com>
2018-02-07 09:02:35 +0100

    Added two abstract methods getInputPattern and getSelectionType

--- src/com/codecool/Value.java
+++ src/com/codecool/Value.java
@@ -3,8 +3,8 @@ package codecool;
 import java.util.List;
 
 public abstract class Value {
-    public List<String> getInputPattern() {
-        return null;
 
-    }
+    public abstract List<String> getInputPattern();
+
+    public abstract boolean getSelectionType();
 }

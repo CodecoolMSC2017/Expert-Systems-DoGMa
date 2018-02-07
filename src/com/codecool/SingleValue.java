8d0dd020bc5744de609c5ed80949646956c4321a
S. Dominik <coderwilk@gmail.com>
2018-02-07 17:54:46 +0100

    SingleValue: Changed the order of statements. Implemented getInputPattern.

--- src/com/codecool/SingleValue.java
+++ src/com/codecool/SingleValue.java
@@ -8,13 +8,13 @@ public class SingleValue extends Value {
     private boolean selectionType;
 
     public SingleValue(String param, boolean selectionType) {
+        inputPattern = new LinkedList<String>();
         inputPattern.add(param);
         this.selectionType = selectionType;
-        inputPattern = new LinkedList<String>();
     }
 
     public List<String> getInputPattern() {
-        return null;
+        return inputPattern;
     }
 
     public boolean getSelectionType() {

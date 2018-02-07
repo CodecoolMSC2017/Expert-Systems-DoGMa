78889999872c7f49eeb24216ea44ad3e4ccfa3bb
S. Dominik <coderwilk@gmail.com>
2018-02-07 17:49:54 +0100

    Added private Map<String, Boolean> to store the user's inputs. Implemented getAnswerByQuestion, collectAnswers and evaluate.

--- src/com/codecool/ESProvider.java
+++ src/com/codecool/ESProvider.java
@@ -1,30 +1,73 @@
 package codecool;
 
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class ESProvider {
 
-    FactParser factParser;
-    RuleParser ruleParser;
+    private FactParser factParser;
+    private RuleParser ruleParser;
+
+    private Map<String, Boolean> preferences;
 
 
     // Constructor(s)
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
         this.factParser = factParser;
         this.ruleParser = ruleParser;
+        preferences = new HashMap<String, Boolean>();
     }
 
 
-    // Getter(s)
+    // Getter(s)                                   length
     public boolean getAnswerByQuestion(String questionId) {
-        return true;
+        return preferences.get(questionId);
     }
 
 
     // ESProvider method(s)
     public void collectAnswers() {
+        Iterator<Question> questions = ruleParser.getRuleRepository().getIterator();
 
+        while (questions.hasNext()) {
+            Question question = questions.next();
+            while(true) {
+                try {
+                    Scanner input = new Scanner(System.in);
+                    question.askUser();
+                    boolean evaluatedAsnwer = question.getEvaluatedAsnwer(input.nextLine());
+                    preferences.put(question.getId(), evaluatedAsnwer);
+
+                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println(evaluate());
     }
 
     public String evaluate() {
-        return "";
+        Iterator<Fact> facts = factParser.getFactRepository().getIterator();
+        String chosenSeries = "";
+
+        while (facts.hasNext()) {
+            Fact fact = facts.next();
+            try {
+                for (Map.Entry<String, Boolean> genre : fact.getGenres().entrySet()) {
+
+                    if (genre.getValue() != preferences.get(genre.getKey())) {
+                        throw new Exception("The user won't like this.");
+                    }
+                }
+                chosenSeries += fact.getDescription() + "\n";
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return chosenSeries;
     }
 }
